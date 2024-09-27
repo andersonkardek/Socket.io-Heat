9b688c714f00a11f0cbdd3a00ef4e9a21619b81d
anderson <kardek.anderson@gmail.com>
2024-09-27 05:35:49 -0400

    feat: starting components

---
+++ heat-web/src/components/LoginBox/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss"
+
+export function LoginBox() {
+	return (
+		<div className={styles.loginBoxWrapper}>
+			<strong>Entre e compartilhe sua mensagem</strong>
+		</div>
+	)
+}
---
+++ heat-web/src/components/MessageList/index.tsx
@@ -0,0 +1,3 @@
+export function MessageList() {
+	return <h1>message</h1>
+}
